30a61f33bc608155f23deefa3cee11ced0eea0c0
Cosmin Udroiu <cosmin.udroiu@c-s.ro>
2017-07-19 15:40:09 +0300

    Initial version of service registry and some skeleton interfaces for topology management.

---
+++ service-registry/src/main/java/ro/cs/tao/spi/DefaultResourceLocator.java
@@ -0,0 +1,70 @@
+package ro.cs.tao.spi;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.FileSystemNotFoundException;
+import java.nio.file.FileSystems;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Enumeration;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * The default resource locator. Locates resources using the current context class loader.
+ * Can be easily subclassed by overriding {@link #getResourceClassLoader()} in order to return an appropriate class loader.
+ */
+public class DefaultResourceLocator extends ResourceLocator {
+
+    public static final Logger LOG = Logger.getLogger(DefaultResourceLocator.class.getName());
+
+    @Override
+    public Collection<Path> locateResources(String name) {
+        Set<Path> resourcesSet = new HashSet<>();
+        try {
+            Enumeration<URL> resources = getResourceClassLoader().getResources(name);
+            while (resources.hasMoreElements()) {
+                URL resource = resources.nextElement();
+                URI uri = toUri(resource);
+                if (uri != null) {
+                    resourcesSet.add(toPath(uri));
+                }
+            }
+        } catch (IOException e) {
+            LOG.log(Level.WARNING, String.format("Failed to retrieve resources for name '%s'", name), e);
+        }
+        return resourcesSet;
+    }
+
+    /**
+     * The default implementation returns the context class loader.
+     * @return the class loader used to load resources.
+     */
+    protected ClassLoader getResourceClassLoader() {
+        return Thread.currentThread().getContextClassLoader();
+    }
+
+    protected Path toPath(URI uri) throws IOException {
+        try {
+            return Paths.get(uri);
+        } catch (FileSystemNotFoundException exp) {
+            FileSystems.newFileSystem(uri, Collections.emptyMap());
+            return Paths.get(uri);
+        }
+    }
+
+    private URI toUri(URL resource) {
+        try {
+            return resource.toURI();
+        } catch (URISyntaxException e) {
+            LOG.log(Level.WARNING, String.format("Ignoring malformed resource URI '%s'", resource), e);
+            return null;
+        }
+    }
+}
---
+++ service-registry/src/main/java/ro/cs/tao/spi/ResourceLocator.java
@@ -0,0 +1,43 @@
+package ro.cs.tao.spi;
+
+import java.nio.file.Path;
+import java.util.Collection;
+import java.util.ServiceLoader;
+
+/**
+ * Resource locators are service providers used collect resources across multiple code bases in multi-module environments.
+ *
+ * @author Norman Fomferra
+ * @since Ceres 2.0
+ */
+public abstract class ResourceLocator {
+
+    /**
+     * Gets all resources from all registered {@link ResourceLocator} services for the given name.
+     * @param name The resource name
+     * @return The collection of all resources found
+     */
+    public static Collection<Path> getResources(String name) {
+        ServiceLoader<ResourceLocator> providers = ServiceLoader.load(ResourceLocator.class);
+        ResourceLocator resourceLocator = null;
+        for (ResourceLocator provider : providers) {
+            if (!provider.getClass().equals(DefaultResourceLocator.class)) {
+                resourceLocator = provider;
+                break;
+            } else if (resourceLocator == null) {
+                resourceLocator = provider;
+            }
+        }
+        if (resourceLocator == null) {
+            resourceLocator = new DefaultResourceLocator();
+        }
+        return resourceLocator.locateResources(name);
+    }
+
+    /**
+     * Locates all resources with the given name.
+     * @param name The resource name
+     * @return The collection of all resources located
+     */
+    public abstract Collection<Path> locateResources(String name);
+}
