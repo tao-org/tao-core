a5e1e768e0ec954a254d10a94baddfca9ab668c2
Cosmin Udroiu <cosmin.udroiu@c-s.ro>
2017-07-20 09:45:40 +0300

    Correction for compilation error.

--- service-registry/src/main/java/ro/cs/tao/spi/ServiceLoader.java
+++ service-registry/src/main/java/ro/cs/tao/spi/ServiceLoader.java
@@ -13,7 +13,7 @@
  * You should have received a copy of the GNU General Public License along
  * with this program; if not, see http://www.gnu.org/licenses/
  */
-package ro.cs.tao.spi_new;
+package ro.cs.tao.spi;
 
 import java.util.Iterator;
 import java.util.ServiceConfigurationError;
