6088c18741ad7f7615f4546a79954af9b2baca39
Alexandru ILIOIU <alexandru.ilioiu@c-s.ro>
2017-07-21 18:56:40 +0300

    add package tao-integration

---
+++ tao-common/src/main/java/ro/cs/tao/exceptions/TaoBaseException.java
@@ -0,0 +1,44 @@
+package ro.cs.tao.exceptions;
+
+/**
+ * @author Alexandru Ilioiu - 19/07/2017
+ */
+public abstract class TaoBaseException extends Exception {
+    private static final long serialVersionUID = -2105876605890377083L;
+
+    private final String messageID;
+
+    private final String[] parameters;
+
+    /**
+     * Constructs an TaoBaseException.
+     *
+     * @param cause the exception at origin
+     * @param messageID the message identifier
+     * @param parameters the message parameters
+     */
+    public TaoBaseException(final Throwable cause, final String messageID, final String... parameters) {
+        super(cause);
+        this.messageID = messageID;
+        this.parameters = parameters;
+    }
+
+    /**
+     * Constructs an TaoBaseException.
+     *
+     * @param messageID the message identifier
+     * @param parameters the message parameters
+     */
+    public TaoBaseException(final String messageID, final String... parameters) {
+        this.messageID = messageID;
+        this.parameters = parameters;
+    }
+
+    public String getMessageID() {
+        return messageID;
+    }
+
+    public String[] getParameters() {
+        return parameters;
+    }
+}
---
+++ tao-integration/pc-deployment/src/main/java/ro/cs/tao/integration/ComponentDeploymentManager.java
@@ -0,0 +1,14 @@
+package ro.cs.tao.integration;
+
+import ro.cs.tao.component.Component;
+import ro.cs.tao.topology.NodeDescription;
+
+/**
+ * @author Alexandru Ilioiu - 19/07/2017
+ *
+ * Manager responsible to deploy a {@link Component} on a {@link NodeDescription}
+ */
+public class ComponentDeploymentManager {
+
+
+}
