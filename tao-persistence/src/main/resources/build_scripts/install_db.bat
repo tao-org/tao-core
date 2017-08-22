a0811c5c851ae8d978656669feb5b7db412bc2a3
Alexandru ILIOIU <alexandru.ilioiu@c-s.ro>
2017-08-22 20:01:09 +0300

    correction for path with spaces (powershell)

--- tao-persistence/src/main/resources/build_scripts/install_db.bat
+++ tao-persistence/src/main/resources/build_scripts/install_db.bat
@@ -2,7 +2,7 @@ echo off
 
 SET mypath=%~dp0
 
-powershell -ExecutionPolicy ByPass -File %mypath%\install_db.ps1
+powershell -ExecutionPolicy ByPass -File "%mypath%\install_db.ps1"
 
 REM set /p DUMMY=Hit ENTER to continue...
 
