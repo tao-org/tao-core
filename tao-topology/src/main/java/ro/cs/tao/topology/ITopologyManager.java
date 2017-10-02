28579e89bbc9984ca374cc7b2273b32df9ce5ae7
oana <oana.hogoiu@c-s.ro>
2017-10-02 11:33:43 +0300

    Topology - refactor methods signature

--- tao-topology/src/main/java/ro/cs/tao/topology/ITopologyManager.java
+++ tao-topology/src/main/java/ro/cs/tao/topology/ITopologyManager.java
@@ -6,9 +6,9 @@ import java.util.List;
  * Created by cosmin on 7/17/2017.
  */
 public interface ITopologyManager {
-    NodeDescription get(String name) throws TopologyException;
+    NodeDescription get(String hostName) throws TopologyException;
     List<NodeDescription> list() throws TopologyException;
     void add(NodeDescription info) throws TopologyException;
     void update(NodeDescription nodeInfo) throws TopologyException;
-    void remove(String name) throws TopologyException;
+    void remove(String hostName) throws TopologyException;
 }
