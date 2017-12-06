d1756c0141bb28f828652a7da2f3d35d332d0f7f
Cosmin Cara <kraftek@c-s.ro>
2017-12-06 17:58:11 +0200

    PEPS download according to specs (may not work)

--- tao-peps/src/test/java/PepsDataSourceTest.java
+++ tao-peps/src/test/java/PepsDataSourceTest.java
@@ -1,15 +1,16 @@
 import ro.cs.tao.datasource.DataQuery;
 import ro.cs.tao.datasource.DataSource;
+import ro.cs.tao.datasource.ProductFetchStrategy;
 import ro.cs.tao.datasource.QueryException;
 import ro.cs.tao.datasource.param.QueryParameter;
 import ro.cs.tao.datasource.remote.peps.Collection;
 import ro.cs.tao.datasource.remote.peps.PepsDataSource;
 import ro.cs.tao.eodata.EOProduct;
-import ro.cs.tao.eodata.Polygon2D;
 import ro.cs.tao.serialization.SerializationException;
 import ro.cs.tao.spi.ServiceRegistry;
 import ro.cs.tao.spi.ServiceRegistryManager;
 
+import java.io.IOException;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.util.Date;
@@ -45,35 +46,34 @@ public class PepsDataSourceTest {
             query.addParameter("platform", "S2A");
 
             QueryParameter begin = query.createParameter("startDate", Date.class);
-            begin.setValue(Date.from(LocalDateTime.of(2017, 2, 1, 0, 0, 0, 0)
+            begin.setValue(Date.from(LocalDateTime.of(2017, 12, 1, 0, 0, 0, 0)
                                                 .atZone(ZoneId.systemDefault())
                                                 .toInstant()));
             query.addParameter(begin);
             QueryParameter end = query.createParameter("completionDate", Date.class);
-            end.setValue(Date.from(LocalDateTime.of(2017, 3, 1, 0, 0, 0, 0)
+            end.setValue(Date.from(LocalDateTime.of(2017, 12, 6, 0, 0, 0, 0)
                                                 .atZone(ZoneId.systemDefault())
                                                 .toInstant()));
             query.addParameter(end);
-            Polygon2D aoi = Polygon2D.fromWKT("POLYGON((22.8042573604346 43.8379609098684," +
+            /*Polygon2D aoi = Polygon2D.fromWKT("POLYGON((22.8042573604346 43.8379609098684," +
                                                       "24.83885442747927 43.8379609098684," +
                                                       "24.83885442747927 44.795645304033826," +
                                                       "22.8042573604346 44.795645304033826," +
                                                       "22.8042573604346 43.8379609098684))");
 
             QueryParameter aoiParam = query.createParameter("box", Polygon2D.class);
-            aoiParam.setValue(aoi.toWKT());
-            query.addParameter(aoiParam);
+            aoiParam.setValue(aoi);
+            query.addParameter(aoiParam);*/
+            query.addParameter("tileid", "34TFP");
 
             query.addParameter("cloudCover", 100.);
             query.setPageSize(20);
-            query.setMaxResults(50);
+            query.setMaxResults(2);
 
-            String xml = query.exportParametersAsXML();
+            /*String xml = query.exportParametersAsXML();
             System.out.println(xml);
+            query.importParameters(xml);*/
 
-            query.importParameters(xml);
-            xml = query.exportParametersAsXML();
-            System.out.println(xml);
             List<EOProduct> results = query.execute();
             results.forEach(r -> {
                 System.out.println("ID=" + r.getId());
@@ -84,6 +84,14 @@ public class PepsDataSourceTest {
                 r.getAttributes().stream()
                   .forEach(a -> System.out.println("\tName='" + a.getName() +
                     "', value='" + a.getValue() + "'"));
+            });
+            final ProductFetchStrategy strategy = dataSource.getProductFetchStrategy("Sentinel-2");
+            results.forEach(r -> {
+                try {
+                    strategy.fetch(r);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
             });
         } catch (QueryException e) {
             e.printStackTrace();
