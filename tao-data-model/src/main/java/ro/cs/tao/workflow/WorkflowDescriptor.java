1ab4f3e5cf3475959d0c913b3399ec8c9a034952
oana <oana.hogoiu@c-s.ro>
2018-02-01 13:09:02 +0200

    persistence tables updates for workflow status

--- tao-data-model/src/main/java/ro/cs/tao/workflow/WorkflowDescriptor.java
+++ tao-data-model/src/main/java/ro/cs/tao/workflow/WorkflowDescriptor.java
@@ -8,5 +8,45 @@ public class WorkflowDescriptor {
     private String name;
     private String userName;
     private Visibility visibility;
+    private Status status;
 
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public Visibility getVisibility() {
+        return visibility;
+    }
+
+    public void setVisibility(Visibility visibility) {
+        this.visibility = visibility;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
 }
