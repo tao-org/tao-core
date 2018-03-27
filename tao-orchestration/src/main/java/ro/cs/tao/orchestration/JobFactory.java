1ff3d256dcca10726a6a509103eeea49aa671105
Cosmin Cara <kraftek@c-s.ro>
2018-03-27 11:09:53 +0300

    Small refactoring of responsibilities

---
+++ tao-orchestration/src/main/java/ro/cs/tao/orchestration/JobFactory.java
@@ -0,0 +1,110 @@
+/*
+ * Copyright (C) 2017 CS ROMANIA
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation; either version 3 of the License, or (at your option)
+ * any later version.
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, see http://www.gnu.org/licenses/
+ */
+
+package ro.cs.tao.orchestration;
+
+import ro.cs.tao.component.ComponentLink;
+import ro.cs.tao.component.ProcessingComponent;
+import ro.cs.tao.component.TaoComponent;
+import ro.cs.tao.datasource.DataSourceComponent;
+import ro.cs.tao.execution.model.*;
+import ro.cs.tao.persistence.PersistenceManager;
+import ro.cs.tao.persistence.exception.PersistenceException;
+import ro.cs.tao.workflow.ParameterValue;
+import ro.cs.tao.workflow.WorkflowDescriptor;
+import ro.cs.tao.workflow.WorkflowNodeDescriptor;
+import ro.cs.tao.workflow.WorkflowNodeGroupDescriptor;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public class JobFactory {
+
+    private final PersistenceManager persistenceManager;
+
+    JobFactory(PersistenceManager persistenceManager) {
+        this.persistenceManager = persistenceManager;
+    }
+
+    public ExecutionJob createJob(WorkflowDescriptor workflow) throws PersistenceException {
+        ExecutionJob job = null;
+        if (workflow != null && workflow.isActive()) {
+            job = new ExecutionJob();
+            job.setUserName("admin");
+            job.setStartTime(LocalDateTime.now());
+            job.setWorkflowId(workflow.getId());
+            job.setExecutionStatus(ExecutionStatus.UNDETERMINED);
+            job = persistenceManager.saveExecutionJob(job);
+            List<WorkflowNodeDescriptor> nodes = workflow.getOrderedNodes();
+            for (WorkflowNodeDescriptor node : nodes) {
+                ExecutionTask task = createTask(node);
+                task.setLevel(node.getLevel());
+                persistenceManager.saveExecutionTask(task, job);
+                System.out.println(String.format("Created task for node %s [level %s]", node.getId(), task.getLevel()));
+            }
+        }
+        return job;
+    }
+
+    public ExecutionTask createTaskGroup(WorkflowNodeGroupDescriptor groupNode) throws PersistenceException {
+        ExecutionGroup group = new ExecutionGroup();
+        List<WorkflowNodeDescriptor> nodes = groupNode.getOrderedNodes();
+        for (WorkflowNodeDescriptor node : nodes) {
+            ExecutionTask task = createTask(node);
+            group.addTask(task);
+        }
+        group.setExecutionStatus(ExecutionStatus.UNDETERMINED);
+        return group;
+    }
+
+    public ExecutionTask createTask(WorkflowNodeDescriptor workflowNode) throws PersistenceException {
+        ExecutionTask task;
+        if (workflowNode instanceof WorkflowNodeGroupDescriptor) {
+            task = createTaskGroup((WorkflowNodeGroupDescriptor) workflowNode);
+        } else {
+            TaoComponent component;
+            List<ParameterValue> customValues = workflowNode.getCustomValues();
+            List<ComponentLink> links = workflowNode.getIncomingLinks();
+            if (links != null) {
+                task = new ProcessingExecutionTask();
+                component = persistenceManager.getProcessingComponentById(workflowNode.getComponentId());
+                ((ProcessingExecutionTask) task).setComponent((ProcessingComponent) component);
+                links.forEach(link -> {
+                    String name = link.getOutput().getName();
+                    String value = link.getInput().getDataDescriptor().getLocation();
+                    task.setParameterValue(name, value);
+                });
+            } else {
+                task = new DataSourceExecutionTask();
+                component = persistenceManager.getDataSourceInstance(workflowNode.getComponentId());
+                ((DataSourceExecutionTask) task).setComponent((DataSourceComponent) component);
+            }
+            task.setWorkflowNodeId(workflowNode.getId());
+            if (customValues != null) {
+                for (ParameterValue customValue : customValues) {
+                    task.setParameterValue(customValue.getParameterName(), customValue.getParameterValue());
+                }
+            }
+            String nodeAffinity = component.getNodeAffinity();
+            if (nodeAffinity != null && !"Any".equals(nodeAffinity)) {
+                task.setExecutionNodeHostName(nodeAffinity);
+            }
+            task.setExecutionStatus(ExecutionStatus.UNDETERMINED);
+        }
+        return task;
+    }
+
+}
--- tao-orchestration/src/main/java/ro/cs/tao/orchestration/commands/JobCommand.java
+++ tao-orchestration/src/main/java/ro/cs/tao/orchestration/commands/JobCommand.java
@@ -13,7 +13,7 @@
  * You should have received a copy of the GNU General Public License along
  * with this program; if not, see http://www.gnu.org/licenses/
  */
-package ro.cs.tao.orchestration;
+package ro.cs.tao.orchestration.commands;
 
 import ro.cs.tao.execution.ExecutionException;
 import ro.cs.tao.execution.model.ExecutionJob;
