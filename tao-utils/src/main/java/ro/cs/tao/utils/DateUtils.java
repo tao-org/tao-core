9b94abcc88b4164cc2bb731fec30407efc182903
Cosmin Cara <kraftek@c-s.ro>
2018-08-02 20:09:08 +0300

    Utility class for various Date/Instant/Duration/LocalDateTime operations and formatting

---
+++ tao-utils/src/main/java/ro/cs/tao/utils/DateUtils.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright (C) 2017 CS ROMANIA
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation; either version 3 of the License, or (at your option)
+ * any later version.
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, see http://www.gnu.org/licenses/
+ */
+
+package ro.cs.tao.utils;
+
+import java.time.Duration;
+
+public class DateUtils {
+
+    public static String formatDuration(Duration duration) {
+        if (duration == null) {
+            return "--h--m--s";
+        }
+        long seconds = duration.getSeconds();
+        return seconds < 3600 ? String.format("%02dm%02ds", seconds / 60, seconds % 60) :
+                String.format("%02dh%02dm%02ds", seconds / 3600, (seconds % 3600) / 60, seconds % 60);
+    }
+
+}
