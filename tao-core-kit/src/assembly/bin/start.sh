08d5e5ab2299bbc21f233055676763c6f32eeee8
Cosmin Cara <kraftek@c-s.ro>
2018-08-22 19:06:18 +0300

    Further changes

--- tao-core-kit/src/assembly/bin/start.sh
+++ tao-core-kit/src/assembly/bin/start.sh
@@ -4,7 +4,7 @@ MYPWD=`pwd`
 SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
 
 cd ${SCRIPTPATH}/..
-java -cp 'config/*:services/*:modules/*:plugins/*:lib/*:/static/*' ro.cs.tao.services.TaoServicesStartup
+java -cp 'config/*:services/*:modules/*:plugins/*:lib/*:static/*' ro.cs.tao.services.TaoServicesStartup
 
 cd ${MYPWD}
 
