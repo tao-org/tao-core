3c624c78f5f76de28bd30d8da0b6444b547dc90c
kraftek <kraftek@c-s.ro>
2020-01-15 17:16:52 +0200

    Changes from Wed 01/15/2020 17:16:51.62

--- tao-service-api/src/main/java/ro/cs/tao/services/interfaces/TAOService.java
+++
@@ -1,9 +0,0 @@
-package ro.cs.tao.services.interfaces;
-
-import ro.cs.tao.configuration.ConfigurationManager;
-
-public interface TAOService {
-    default boolean isDevModeEnabled() {
-        return Boolean.parseBoolean(ConfigurationManager.getInstance().getValue("tao.dev.mode", "false"));
-    }
-}
